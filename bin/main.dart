ef5686f2a933e3e282197da722c67a664f615975
weslley41 <41weslley@gmail.com>
2022-07-28 18:49:44 -0300

    testing CSVData

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,20 @@
+import 'package:data_transfer_formats/csv_data.dart';
 
 void main() {
+  CSVData csvData = CSVData();
+
+  csvData.load('examples/example.csv');
+  print("*data fields*");
+  print(csvData.fields);
+  print("*data content*");
+  print(csvData.data);
+
+  /*
+  csvData.clear();
+  print("\n*clear data*");
+  print(csvData.fields);
+  print(csvData.data);
+  */
+
+  csvData.save('examples_saves/example.csv');
 }
