6bd0987a46bb421070b973f3cb3919be29e2c1dd
weslley41 <41weslley@gmail.com>
2022-07-28 12:09:26 -0300

    modify abstract classes for add default attributes/methods

--- lib/data.dart
+++ lib/data.dart
@@ -1,13 +1,20 @@
 
 abstract class Data {
 
+  List<String> _fields = [];
+  String _data = '';
+
   void load(String fileName);
   void save(String fileName);
-  void clear();
+  void clear() {
+    _fields = [];
+    _data = '';
+  }
 
-  set data(String data) => data;
-  bool get hasData;
-  String get data;
-  List<String> get fields;
+  set data(String data);
+  set fields(List<String> fields);
+  String get data => _data;
+  List<String> get fields => _fields;
+  bool get hasData => _data.isNotEmpty;
 
 }
--- lib/delimeted_data.dart
+++ lib/delimeted_data.dart
@@ -1,7 +1,11 @@
-import 'data.dart';
+import 'package:data_transfer_formats/data.dart';
 
 abstract class DelimetedData extends Data {
 
-  String get separator;
+  final String _separator;
+
+  DelimetedData({required separator}) : _separator = separator;
+
+  String get separator => _separator;
 
 }
