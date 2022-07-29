68bab77f7f63477a229d063a52b47d025c3e584f
weslley41 <41weslley@gmail.com>
2022-07-29 10:00:59 -0300

    Update tests of CSVData

--- test/csv_data_test.dart
+++ test/csv_data_test.dart
@@ -1,12 +1,16 @@
+import 'dart:io';
 import 'package:test/test.dart';
 
 import 'package:data_transfer_formats/csv_data.dart';
 
 void main() {
+  const inputFileTest = 'examples/example.csv';
+  const outputFileTest = 'examples_saves/example.csv';
+
   test('InitializeCSVDataClass', () {
-    final csvDataClass = CSVData();
-    expect(csvDataClass.data, '');
-    expect(csvDataClass.fields, []);
+    final csvData = CSVData();
+    expect(csvData.data, '');
+    expect(csvData.fields, []);
   });
 
   test('CSVDataGetterSeparator', () {
@@ -15,5 +19,30 @@ void main() {
 
   test('CSVDataHasDataFalse', () {
     expect(CSVData().hasData, false);
+  });
+
+  test('CSVDataLoadFile', () {
+    final csvData = CSVData();
+    csvData.load(inputFileTest);
+
+    expect(csvData.hasData, true);
+  });
+
+  test('CSVDataClearData', () {
+    final csvData = CSVData();
+    csvData.load(inputFileTest);
+    csvData.clear();
+
+    expect(csvData.hasData, false);
+    expect(csvData.fields, []);
+  });
+
+  test('CSVDataSaveDataInFile', () {
+    final csvData = CSVData();
+    csvData.load(inputFileTest);
+    csvData.save(outputFileTest);
+    File outputFile = File(outputFileTest);
+
+    expect(csvData.data, outputFile.readAsStringSync());
   });
 }
