65c330f92e3fb9b4a3175913ad71c21358ed67d8
weslley41 <41weslley@gmail.com>
2022-07-29 10:55:36 -0300

    move CSVData content for abstract class DelimitedData

--- lib/csv_data.dart
+++ lib/csv_data.dart
@@ -1,51 +1,5 @@
-import 'package:data_transfer_formats/delimeted_data.dart';
+import 'package:data_transfer_formats/delimited_data.dart';
 
-class CSVData extends DelimetedData {
+class CSVData extends DelimitedData {
   CSVData() : super(separator: ',');
-
-  @override
-  set data(String data) {
-    List<String> splitData = data.split('\n');
-
-    for (int indexRow = 0; indexRow < splitData.length; indexRow++) {
-      List<String> row = splitData[indexRow].replaceAll('"', '').split(',');
-
-      if (indexRow == 0) {
-        fields = row;
-        continue;
-      }
-
-      Map<String, dynamic> mapRow = {};
-      for (int i = 0; i < fields.length; i++) {
-        mapRow[fields[i]] = row[i];
-      }
-
-      dataSet.add(mapRow);
-    }
-  }
-
-  @override
-  String get data {
-    if (!hasData) return '';
-
-    List<String> fieldsWithQuotes = fields.map((field) => '"$field"').toList();
-    String dataString = fieldsWithQuotes.join(separator);
-
-    for (Map row in dataSet) {
-      List<dynamic> rowWithQuotes = row.values.map((value) =>
-        num.tryParse(value) != null ? value : '"$value"'
-      ).toList();
-
-      dataString += "\n${rowWithQuotes.join(',')}";
-    }
-
-    return dataString;
-  }
-
-  @override
-  set fields(List<String> fields) => fieldsList = fields;
-
-  @override
-  List<String> get fields => fieldsList;
-
 }
---
+++ lib/delimited_data.dart
@@ -0,0 +1,56 @@
+import 'package:data_transfer_formats/data.dart';
+
+abstract class DelimitedData extends Data {
+
+  final String _separator;
+
+  DelimitedData({required separator}) : _separator = separator;
+
+  String get separator => _separator;
+
+  @override
+  set data(String data) {
+    List<String> splitData = data.split('\n');
+
+    for (int indexRow = 0; indexRow < splitData.length; indexRow++) {
+      List<String> row = splitData[indexRow].replaceAll('"', '').split(separator);
+
+      if (indexRow == 0) {
+        fields = row;
+        continue;
+      }
+
+      Map<String, dynamic> mapRow = {};
+      for (int i = 0; i < fields.length; i++) {
+        mapRow[fields[i]] = row[i];
+      }
+
+      dataSet.add(mapRow);
+    }
+  }
+
+  @override
+  String get data {
+    if (!hasData) return '';
+
+    List<String> fieldsWithQuotes = fields.map((field) => '"$field"').toList();
+    String dataString = fieldsWithQuotes.join(separator);
+
+    for (Map row in dataSet) {
+      List<dynamic> rowWithQuotes = row.values.map((value) =>
+        num.tryParse(value) != null ? value : '"$value"'
+      ).toList();
+
+      dataString += "\n${rowWithQuotes.join(separator)}";
+    }
+
+    return dataString;
+  }
+
+  @override
+  set fields(List<String> fields) => fieldsList = fields;
+
+  @override
+  List<String> get fields => fieldsList;
+
+}
