e397fcaff6bc48bde124f44e0bffb1ee0642b1b9
weslley41 <41weslley@gmail.com>
2022-07-31 17:00:58 -0300

    change main file

--- bin/data_transfer_formats.dart
+++ bin/data_transfer_formats.dart
@@ -1,5 +1,41 @@
-import 'package:data_transfer_formats/main.dart' as program;
 
-void main (List<String> args) {
-  program.main();
+import 'package:data_transfer_formats/datas.dart';
+
+void main() {
+  CSVData csvData = CSVData();
+
+  csvData.load('examples/example.csv');
+  print("*data fields*");
+  print(csvData.fields);
+  /*
+  print("*data content*");
+  print(csvData.data);
+
+  csvData.clear();
+  print("\n*clear data*");
+  print(csvData.fields);
+  print(csvData.data);
+
+  csvData.save('examples_saves/example.csv');
+  */
+
+  
+  // XMLData xmlData = XMLData();
+  // xmlData.load('examples/example.xml');
+  // // print("*xml data*");
+  // // print(xmlData.data);
+  // print("\n*xml fields*");
+  // print(xmlData.fields);
+  
+
+  // JSONData jsonData = JSONData();
+  // jsonData.data = '[{}]';
+  // try {
+  //   jsonData.load('examples/example.json');
+  // } catch (e) {
+  //   print(e);
+  // }
+  // jsonData.save('examples_saves/example.json');
+  // print(jsonData.fields);
+
 }
